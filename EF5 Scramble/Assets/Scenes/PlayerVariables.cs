c6e0520a60f2b41a62f3c7b10fbfa730f66c7622
NolanB <nolan.bretz@stu.daviess.kyschools.us>
2026-09-14 13:21:38 -0500

    PlayerVariables

---
+++ EF5 Scramble/Assets/Scenes/PlayerVariables.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+public class Player
+{
+    
+}
+
+
+public class PlayerVariables 
+{
+    public int fearMeter;
+    public Boolean hungerMeter;
+    public Boolean thirstMeter;
+
+    
+}
+
+
+public class PlayerUpdate : PlayerVariables
+{
+    int initial = 0;
+
+}
