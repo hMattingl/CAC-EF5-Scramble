facaa632e154c5327f1df20eba7ca2fd4c24b702
NolanB <nolan.bretz@stu.daviess.kyschools.us>
2026-09-15 12:51:08 -0500

    Player Variables

--- EF5 Scramble/Assets/Scenes/PlayerVariables.cs
+++ EF5 Scramble/Assets/Scenes/PlayerVariables.cs
@@ -2,25 +2,49 @@ using System;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+public class PlayerVariables
+{
+    public static void main()
+    {
+        //This costructs a player named Bob
+        //It gives all of his stats, fear meter, hunger, and thirst. 
+        //Is alive determines at the end of the round if the player character is still
+        // or if you failed and you lose. 
+        Player Bob = new Player();
+        Bob.fearMeter = 0;
+        Bob.hungry = false;
+        Bob.thirsty = false;
+        Bob.isAlive = true;
 
-public class Player
+        //This constructs wife
+        //She has the same stats as player character
+        //however the wife doesn't have to live for you to win the round. 
+        Wife wife = new Wife();
+        wife.fearMeter = 0;
+        wife.hungry = false;
+        wife.thirsty = false;
+        wife.isAlive = true;
+    }
+}
+
+    
+public class Player : Variables
 {
     
 }
 
 
-public class PlayerVariables 
+public class Variables 
 {
     public int fearMeter;
-    public Boolean hungerMeter;
-    public Boolean thirstMeter;
+    public Boolean hungry;
+    public Boolean thirsty;
+    public Boolean isAlive;
 
-    
+
 }
 
-
-public class PlayerUpdate : PlayerVariables
+public class Wife : Variables
 {
-    int initial = 0;
-
+    
 }
