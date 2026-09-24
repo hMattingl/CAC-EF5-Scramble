c1317631524ebe2de9d279e649d584ff10a8b380
Hayden-Mattingly <hayden.mattingly2@stu.daviess.kyschools.us>
2026-09-24 13:09:35 -0500

    random

--- EF5 Scramble/Assets/Scenes/statsDisplay.cs
+++ EF5 Scramble/Assets/Scenes/statsDisplay.cs
@@ -11,8 +11,16 @@ public class statsDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //sets fear to 0 if it goes below 0
+        if (fear < 0)
+        {
+            fear = 0;
+        }
+
+        //displays fear and other vars
         stats.text = " FEAR METER : " + fear + " has radio :" + hasRadio;
 
+        //game over state
         if (fear >= 100)
         {
             SceneManager.LoadScene("GameOver");
@@ -23,5 +31,19 @@ public class statsDisplay : MonoBehaviour
     public void ChangeFearMeter (int ammount)
     {
         fear = fear + ammount;
+    }
+
+    public void RandomEvent()
+    {
+        int rEvent = Random.Range(0, 20);
+
+        if (rEvent == 0)
+        {
+            fear += 10;
+        }
+        else
+        {
+            fear -= 10;
+        }
     }
 }
